2bd789174081508a6dd27c343f9c67b582fdf620
Altsep <coamnv@gmail.com>
2022-03-20 03:34:39 +0300

    Change input button text, realign controls a bit, update tailwind

--- src/Controls.js
+++ src/Controls.js
@@ -4,20 +4,19 @@ function Controls({ text, length, setLength, convert, setError }) {
   const inputRef = useRef(null);
   const [showInput, setShowInput] = useState(false);
   const type = text.split(' ')[0].toLowerCase();
-  const minValue = type === 'break' ? 2 : type === 'session' && 5;
-  const maxValue = type === 'break' ? 30 : type === 'session' && 180;
-  const secondsInMinute = 60;
   useEffect(() => {
     const item = +localStorage.getItem(type);
     item && setLength(item);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  const errorTextMin = 'Value is at its minimum';
-  const errorTextMax = 'Value is at its maximum';
-  const errorTextEmpty = 'Input field is empty';
   useEffect(() => {
     localStorage.setItem(type, length);
   }, [length, type]);
+  const minValue = type === 'break' ? 2 : type === 'session' && 5;
+  const maxValue = type === 'break' ? 30 : type === 'session' && 180;
+  const secondsInMinute = 60;
+  const errorTextMin = 'Value set to it\'s minimum';
+  const errorTextMax = 'Value set to it\'s maximum';
+  const errorTextEmpty = 'Input field is empty';
   const handleLength = () => {
     const vStr = inputRef.current.value;
     const v = +vStr;
@@ -51,8 +50,11 @@ function Controls({ text, length, setLength, convert, setError }) {
             -
           </button>
         </div>
-        <div className='font-mono' id='session-length'>
-          {convert(length * secondsInMinute, 'min')}
+        <div className='flex' id='session-length'>
+          <p className='font-mono'>
+            {convert(length * secondsInMinute, 'min')}
+          </p>
+          <p className='text-sm mb-1 self-end'>m</p>
         </div>
         <div id='session-increment'>
           <button
@@ -70,17 +72,6 @@ function Controls({ text, length, setLength, convert, setError }) {
           className='flex row items-center children:mx-1 children:rounded-md'
           id='session-set'
         >
-          <button
-            className='flex justify-center items-center bg-gray-100 hover:bg-white p-2 w-20 h-10 md:w-min md:h-5 text-lg font-thin shadow-md'
-            onClick={(e) => {
-              setShowInput((s) => !s);
-              const t = e.target.innerText;
-              if (t === 'Confirm') handleLength();
-              else setTimeout(() => inputRef.current.focus(), 1);
-            }}
-          >
-            {showInput ? 'Confirm' : 'Set'}
-          </button>
           <div className={(!showInput && 'hidden') + ' flex row'}>
             <input
               ref={inputRef}
@@ -99,8 +90,18 @@ function Controls({ text, length, setLength, convert, setError }) {
                 }
               }}
             />
-            <p className='text-sm self-end mx-1'>minutes</p>
           </div>
+          <button
+            className='flex justify-center items-center bg-gray-100 hover:bg-white p-2 w-20 h-10 md:w-min md:h-5 text-lg font-thin shadow-md'
+            onClick={(e) => {
+              setShowInput((s) => !s);
+              const t = e.target.innerText;
+              if (t === 'Confirm') handleLength();
+              else setTimeout(() => inputRef.current.focus(), 1);
+            }}
+          >
+            {showInput ? 'Confirm' : 'Input'}
+          </button>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,10 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
-    screens: {
-      xs: '414px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px',
-    },
   },
   variants: {
     extend: {
-      margin: ['active'],
-      padding: ['active'],
-      width: ['active'],
-      height: ['active'],
-      boxShadow: ['active'],
-      translate: ['active'],
-      scale: ['active'],
       display: [
         'children',
         'DEFAULT',
